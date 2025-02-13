66b24250e48444276885a39dd1962c6ad8035265
Ramilson <ramilsonfelixsilva9@gmail.com>
2025-02-13 23:44:18 +0000

    Publish 4 from staging into origin/main

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -113,8 +113,8 @@ export default function Footer({
           <div>
             <Image
               src={logo.src || ""}
-              width={100}
-              height={28}
+              width={150}
+              height={150}
               alt={logo.alt}
             />
           </div>
